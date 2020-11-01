87bec46ac71d780850f180b323f27986209ec8c2
Nick C <ctwoon@pixelexperience.org>
2020-11-01 20:36:55 +0000

    curtana: remove hotwordenrollment
    This reverts commit ead6fe2b43d50f21bf774670690525e699a65171.
    
    - causes random reboots

--- device.mk
+++ device.mk
@@ -265,11 +265,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# Hotword Enrollment
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
